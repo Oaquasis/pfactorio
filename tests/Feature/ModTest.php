1e142d54372313fe1a2859c24ce79a30b039e6f5
Oaquasis <WbrO#2611>
2018-02-27 16:15:54 +0100

    Adding Tests

--- tests/Feature/ModTest.php
+++ tests/Feature/ModTest.php
@@ -9,15 +9,50 @@ class ModTest extends TestCase
 {
     use DatabaseMigrations;
 
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function a_user_can_browse_mods()
-    {
-        $response = $this->get('/');
+    protected $mod;
 
-        $response->assertStatus(200);
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->mod = factory('pfactorio\Mod')->create();
     }
+
+    /** @test */
+    public function a_guest_cannot_view_all_mods()
+    {
+        $this->expectException('Illuminate\Auth\AuthenticationException');
+
+        $this->get('/mod/')
+             ->assertSee($this->mod->name);
+    }
+
+    /** @test */
+    public function an_authenticated_user_can_view_all_mods()
+    {
+        $this->signIn();
+
+        $this->get('/mod')
+             ->assertSee($this->mod->name);
+    }
+
+    /** @test */
+    public function a_guest_cannot_view_a_single_mod()
+    {
+        $this->expectException('Illuminate\Auth\AuthenticationException');
+
+        $this->get('/mod/'.$this->mod->id)
+             ->assertSee($this->mod->name);
+    }
+
+    /** @test */
+    public function an_authenticated_can_view_a_single_mod()
+    {
+        $this->signIn();
+
+        $this->get('/mod/'.$this->mod->id)
+             ->assertSee($this->mod->name);
+    }
+
+
 }
